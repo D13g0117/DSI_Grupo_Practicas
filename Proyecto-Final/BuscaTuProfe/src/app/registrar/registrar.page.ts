08e8069450f449b7f71456901185c50f5b6bd1fd
Diego <diegogg117@gmail.com>
2019-03-31 12:42:36 +0200

    Registro

--- Proyecto-Final/BuscaTuProfe/src/app/registrar/registrar.page.ts
+++ Proyecto-Final/BuscaTuProfe/src/app/registrar/registrar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-registrar',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarPage implements OnInit {
 
-  constructor() { }
+  constructor(private afauth : AngularFireAuth, public router: Router) { }
 
+  email: string;
+  password: string;
   ngOnInit() {
+  }
+  async OnRegister(){
+    try{
+      const result = await this.afauth.auth.createUserWithEmailAndPassword(this.email, this.password);
+      console.log(result);
+      this.router.navigate(['/home']);
+    }catch(e){
+      alert(e.message);
+    }
+
   }
 
 }
