ef3bd0954e0dba318dc119da5bf3be948e075892
Alvaro <albusti1996@gmail.com>
2019-04-14 11:21:25 +0200

    Search incompleto
    co_search not found

--- Proyecto-Final/BuscaTuProfe/src/app/app.module.ts
+++ Proyecto-Final/BuscaTuProfe/src/app/app.module.ts
@@ -14,8 +14,9 @@ import { firebaseConfig } from "../environments/environment";
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireAuthModule } from "@angular/fire/auth";
 import { AngularFirestoreModule , FirestoreSettingsToken } from "@angular/fire/firestore";
-import { ConversacionComponent } from "./componentes/conversacion/conversacion.component"
-import {FormsModule} from "@angular/forms"
+import { ConversacionComponent } from "./componentes/conversacion/conversacion.component";
+import {FormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [AppComponent, ConversacionComponent],
@@ -27,7 +28,8 @@ import {FormsModule} from "@angular/forms"
     AppRoutingModule,
   AngularFireModule.initializeApp(firebaseConfig), 
   AngularFireAuthModule,
-  AngularFirestoreModule
+  AngularFirestoreModule,
+  AngularFirestoreModule.enablePersistence()
   ],
   providers: [
     StatusBar,
--- Proyecto-Final/BuscaTuProfe/src/app/search/search.module.ts
+++ Proyecto-Final/BuscaTuProfe/src/app/search/search.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
-
+import { Subject } from 'rxjs/Subject';
 import { SearchPage } from './search.page';
+import { AngularFirestore } from '@angular/fire/firestore';
 
+import { Observable } from 'rxjs/Rx';
 const routes: Routes = [
   {
     path: '',
@@ -23,4 +25,48 @@ const routes: Routes = [
   ],
   declarations: [SearchPage]
 })
-export class SearchPageModule {}
+export class SearchPageModule implements OnInit {
+  searchterm: string;
+
+  startAt = new Subject();
+  endAt = new Subject();
+
+  clubs;
+  allclubs;
+
+  startobs = this.startAt.asObservable();
+  endobs = this.endAt.asObservable();
+  constructor(private afs: AngularFirestore){
+
+  }
+  ngOnInit() {
+    this.getallclubs().subscribe((clubs) => {
+      this.allclubs = clubs;
+    })
+    Observable.combineLatest(this.startobs, this.endobs).subscribe((value) => {
+      this.firequery(value[0], value[1]).subscribe((clubs) => {
+        this.clubs = clubs;
+      })
+    })
+  }
+
+  search($event) {
+    let q = $event.target.value;
+    if (q != '') {
+      this.startAt.next(q);
+      this.endAt.next(q + "\uf8ff");
+    }
+    else {
+      this.clubs = this.allclubs;
+    }
+  }
+
+  firequery(start, end) {
+    return this.afs.collection('perfilRooms', ref => ref.limit(4).orderBy('name').startAt(start).endAt(end)).valueChanges();
+  }
+
+  getallclubs() {
+    return this.afs.collection('perfilRooms', ref => ref.orderBy('name')).valueChanges();
+  }
+
+}
